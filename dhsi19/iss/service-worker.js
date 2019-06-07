ed88362343c1b1421ae18179d65c089f172031c4
oncomouse <oncomouse@gmail.com>
2019-06-07 09:09:45 -0700

    🥺

--- dhsi19/iss/service-worker.js
+++ dhsi19/iss/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f3738e4f34db3fccd204c1351083a5ab.js"
+  "/dhsi19/iss/precache-manifest.58c83c7b46423b58ece3a070b269b7ba.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/dhsi19/iss/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
