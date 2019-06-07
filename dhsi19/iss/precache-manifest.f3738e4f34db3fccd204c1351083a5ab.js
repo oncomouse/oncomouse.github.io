dc6716bcc953b6cd020ed9b2101f36be7a8902e8
oncomouse <oncomouse@gmail.com>
2019-06-07 08:42:24 -0700

    Initial Commit

---
+++ dhsi19/iss/precache-manifest.f3738e4f34db3fccd204c1351083a5ab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a98fb252f1b57edae1837a712110901",
+    "url": "/index.html"
+  },
+  {
+    "revision": "698d858e32eeb5ae27f4",
+    "url": "/static/css/main.52f2af89.chunk.css"
+  },
+  {
+    "revision": "914fd1e35a21de2fc604",
+    "url": "/static/js/2.e9dad019.chunk.js"
+  },
+  {
+    "revision": "698d858e32eeb5ae27f4",
+    "url": "/static/js/main.a248f51d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
