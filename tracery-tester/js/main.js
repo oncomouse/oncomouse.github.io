fdf2a7c956c33e83f1c21ceb825a4ca4211c6d92
oncomouse <oncomouse@gmail.com>
2021-10-04 16:04:28 -0500

    Cleaned up code

--- tracery-tester/js/main.js
+++ tracery-tester/js/main.js
@@ -1,35 +1,45 @@
-$(function() {
+$(function () {
 	var grammarInput = Kefir.fromEvents($("#grammar"), "keyup").map(
 		R.path(["target", "value"])
 	);
 
-	var grammarValidator = grammarInput.flatMap(function (grammar) {
-		var lint = JSONLint(grammar);
-		if (lint.error) {
-			return Kefir.constantError(lint);
-		}
-		return Kefir.constant(grammar);
+	var grammar = grammarInput.flatMap(
+		R.tryCatch(
+			R.pipe(
+				JSON.parse,
+				R.ifElse(
+					R.has("origin"),
+					R.identity,
+					R.tap(function () {
+						throw "Missing origin property";
+					})
+				),
+				Kefir.constant
+			),
+			Kefir.constantError
+		)
+	);
+
+	grammar.onError(function (error) {
+		$("#generate").addClass("disabled");
+		$("#error").html(
+			"<strong>There was an error in your JSON.</strong><br><br>Message: " +
+				error
+		);
+		$("#output").html("");
 	});
 
-	grammarValidator.onError(function(lint) {
-		$('#generate').addClass('disabled');
-		$('#error').html('<strong>There was an error in your JSON.</strong><br><br>Message: ' + lint.error + '(' + lint.line + ':' + lint.character + ')');
-	})
+	grammar.onValue(function () {
+		$("#generate").removeClass("disabled");
+		$("#error").html("");
+	});
 
-	grammarValidator.onValue(function() {
-		$('#generate').removeClass('disabled');
-		$('#error').html('');
-	})
+	var traceryGrammar = grammar.map(tracery.createGrammar).toProperty();
 
-	var traceryGrammar = grammarValidator
-		.map(JSON.parse)
-		.map(tracery.createGrammar)
-		.toProperty();
-
-	var createGrammar = function(t) {
-		$("#output").html(t.flatten('#origin#').replace(/\n/g, "<br>"));
-	}
+	var createGrammar = function (t) {
+		$("#output").html(t.flatten("#origin#").replace(/\n/g, "<br>"));
+	};
 	var clickGrammar = Kefir.fromEvents($("#generate"), "click");
 	traceryGrammar.onValue(createGrammar);
 	traceryGrammar.sampledBy(clickGrammar).onValue(createGrammar);
-})
+});
